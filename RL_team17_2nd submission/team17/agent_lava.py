43651c249686499b5a8ebdc6af882d54e328ec48
Kanghyun Ryu <45212225+kh-ryu@users.noreply.github.com>
2022-06-20 00:56:51 +0900

    Add files via upload

---
+++ RL_team17_2nd submission/team17/agent_lava.py
@@ -0,0 +1,269 @@
+import sys
+import gym
+import torch
+import random
+import numpy as np
+from collections import deque
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import transforms
+
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+class SumTree:
+    write = 0
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree = np.zeros(2 * capacity - 1)
+        self.data = np.zeros(capacity, dtype=object)
+        self.n_entries = 0
+
+    # update to the root node
+    def _propagate(self, idx, change):
+        parent = (idx - 1) // 2
+        self.tree[parent] += change
+
+        if parent != 0:
+            self._propagate(parent, change)
+
+    # find sample on leaf node
+    def _retrieve(self, idx, s):
+        left = 2 * idx + 1
+        right = left + 1
+
+        if left >= len(self.tree):
+            return idx
+
+        if s <= self.tree[left]:
+            return self._retrieve(left, s)
+        else:
+            return self._retrieve(right, s - self.tree[left])
+
+    def total(self):
+        return self.tree[0]
+
+    # store priority and sample
+    def add(self, p, data):
+        idx = self.write + self.capacity - 1
+
+        self.data[self.write] = data
+        self.update(idx, p)
+
+        self.write += 1
+        if self.write >= self.capacity:
+            self.write = 0
+
+        if self.n_entries < self.capacity:
+            self.n_entries += 1
+
+    # update priority
+    def update(self, idx, p):
+        change = p - self.tree[idx]
+
+        self.tree[idx] = p
+        self._propagate(idx, change)
+
+    # get priority and sample
+    def get(self, s):
+        idx = self._retrieve(0, s)
+        dataIdx = idx - self.capacity + 1
+
+        return (idx, self.tree[idx], self.data[dataIdx])
+
+
+class Memory:  # stored as ( s, a, r, s_ ) in SumTree
+    e = 0.01
+    a = 0.6
+    beta = 0.4
+    beta_increment_per_sampling = 0.001
+
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+        self.capacity = capacity
+
+    def _get_priority(self, error):
+        return (np.abs(error) + self.e) ** self.a
+
+    def add(self, error, sample):
+        p = self._get_priority(error)
+        self.tree.add(p, sample)
+
+    def sample(self, n):
+        batch = []
+        idxs = []
+        segment = self.tree.total() / n
+        priorities = []
+
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
+
+        for i in range(n):
+            a = segment * i
+            b = segment * (i + 1)
+            while True:
+                s = random.uniform(a, b)
+                (idx, p, data) = self.tree.get(s)
+                if not isinstance(data, int):
+                    break
+            priorities.append(p)
+            batch.append(data)
+            idxs.append(idx)
+
+        sampling_probabilities = priorities / self.tree.total()
+        is_weight = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
+        is_weight /= is_weight.max()
+
+        return batch, idxs, is_weight
+
+    def update(self, idx, error):
+        p = self._get_priority(error)
+        self.tree.update(idx, p)
+
+
+class DQN(nn.Module):
+    def __init__(self, state_size, output_dim):
+        super(DQN, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(state_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, output_dim)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+
+class agent():
+    def __init__(self, env):
+        self.render = False
+
+        # get size of state and action
+        self.state_size = env.observation_space.n
+        self.action_size = env.action_space.n
+
+        # These are hyper parameters for the DQN
+        self.discount_factor = 0.88
+        self.learning_rate = 0.0003
+        self.memory_size = 20000
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.explore_step = 10000
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / self.explore_step
+        self.batch_size = 256
+        self.train_start = 1000
+
+        # create prioritized replay memory using SumTree
+        self.memory = Memory(self.memory_size)
+
+        # create main model and target model
+        self.model = DQN(self.state_size, self.action_size)
+        self.target_model = DQN(self.state_size, self.action_size)
+        self.optimizer = optim.Adam(self.model.parameters(),
+                                    lr=self.learning_rate)
+
+        # initialize target model
+        self.update_target_model()
+
+    def load_weights(self):
+        self.model = torch.load('lava_agent')
+
+    # after some time interval update the target model to be same with model
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    # get action from model using epsilon-greedy policy
+    def action(self, state, eval_mode=False):
+        if eval_mode:
+            state = torch.from_numpy(state)
+            state = Variable(state).float().cpu()
+            q_value = self.model(state)
+            _, action = torch.max(q_value, 1)
+            return int(action)
+
+        elif np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+
+        else:
+            state = torch.from_numpy(state)
+            state = Variable(state).float().cpu()
+            q_value = self.model(state)
+            _, action = torch.max(q_value, 1)
+            return int(action)
+
+    # save sample (error,<s,a,r,s'>) to the replay memory
+    def append_sample(self, state, action, reward, next_state, done):
+        target = self.model(Variable(torch.FloatTensor(state))).data
+        old_val = target[0][action]
+        target_val = self.target_model(Variable(torch.FloatTensor(next_state))).data
+        if done:
+            target[0][action] = reward
+        else:
+            target[0][action] = reward + self.discount_factor * torch.max(target_val)
+
+        error = abs(old_val - target[0][action])
+
+        self.memory.add(error, (state, action, reward, next_state, done))
+
+    # pick samples from prioritized replay memory (with batch_size)
+    def train_model(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_decay
+
+        mini_batch, idxs, is_weights = self.memory.sample(self.batch_size)
+        mini_batch = np.array(mini_batch, dtype=object).transpose()
+
+        states = np.vstack(mini_batch[0])
+        actions = list(mini_batch[1])
+        rewards = list(mini_batch[2])
+        next_states = np.vstack(mini_batch[3])
+        dones = mini_batch[4]
+
+        # bool to binary
+        dones = dones.astype(int)
+
+        # Q function of current state
+        states = torch.Tensor(states)
+        states = Variable(states).float()
+        pred = self.model(states)
+
+        # one-hot encoding
+        a = torch.LongTensor(actions).view(-1, 1)
+
+        one_hot_action = torch.FloatTensor(self.batch_size, self.action_size).zero_()
+        one_hot_action.scatter_(1, a, 1)
+
+        pred = torch.sum(pred.mul(Variable(one_hot_action)), dim=1)
+
+        # Q function of next state
+        next_states = torch.Tensor(next_states)
+        next_states = Variable(next_states).float()
+        next_pred = self.target_model(next_states).data
+
+        rewards = torch.FloatTensor(rewards)
+        dones = torch.FloatTensor(dones)
+
+        # Q Learning: get maximum Q value at s' from target model
+        target = rewards + (1 - dones) * self.discount_factor * next_pred.max(1)[0]
+        target = Variable(target)
+
+        errors = torch.abs(pred - target).data.numpy()
+
+        # update priority
+        for i in range(self.batch_size):
+            idx = idxs[i]
+            self.memory.update(idx, errors[i])
+
+        self.optimizer.zero_grad()
+
+        # MSE Loss function
+        loss = (torch.FloatTensor(is_weights) * F.mse_loss(pred, target)).mean()
+        loss.backward()
+
+        # and train
+        self.optimizer.step()
